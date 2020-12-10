77569200a39abc35f94b7c2172c73ec27cff8641
tomato <tomatono@gmail.com>
2020-12-11 03:30:46 +0900

    カード追加

---
+++ mod/NewLeader1/NewLeader1/betterrecon_policy_text.sql
@@ -0,0 +1,12 @@
+INSERT INTO EnglishText('Tag', 'Text') VALUES
+	('LOC_POLICY_BETTER_RECON_EARLY_EXPLORATION_NAME', 'Early Exploration'),
+	('LOC_POLICY_BETTER_RECON_CIVILIZED_MILITIA_NAME', 'Civilized Militia'),
+	('LOC_POLICY_BETTER_RECON_NEW_WORLD_EXPLORATION_NAME', 'New World Exploration'),
+	('LOC_POLICY_BETTER_RECON_SPOTTER_NAME', 'Spotters'),
+	('LOC_ABILITY_BETTER_RECON_SPOTTER_NAME', 'Spotters'),
+
+	('LOC_POLICY_BETTER_RECON_EARLY_EXPLORATION_DESC', '+100% [ICON_Production] Production towards Recon units.'),
+	('LOC_POLICY_BETTER_RECON_CIVILIZED_MILITIA_DESC', '+5 [ICON_Strength] Unit Combat Strength when fighting Barbarians. +50% experience for recon units.'),
+	('LOC_POLICY_BETTER_RECON_NEW_WORLD_EXPLORATION_DESC', '+100% [ICON_Production] Production towards Recon units and they gain +50% experience.'),
+	('LOC_POLICY_BETTER_RECON_SPOTTER_DESC', 'Ranged Units next to Recon units gain +1 Sight Range and +1 [ICON_Range] Range.'),
+	('LOC_ABILITY_BETTER_RECON_SPOTTER_DESC', 'Adjacent Range units gain +1 Sight Range and +1 [ICON_Range] Range');
