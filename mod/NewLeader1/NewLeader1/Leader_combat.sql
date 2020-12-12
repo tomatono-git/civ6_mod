0bdf83620f6d8f08f7b22d6f26a28b20a8bc49eb
tomato <tomatono@gmail.com>
2020-12-12 23:54:41 +0900

    カードの変更を廃止、戦闘力UP

---
+++ mod/NewLeader1/NewLeader1/Leader_combat.sql
@@ -0,0 +1,55 @@
+ --	戦闘
+
+INSERT OR REPLACE INTO Modifiers
+		(ModifierId, ModifierType)
+	VALUES
+		('TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_COMBAT', 'MODIFIER_PLAYER_UNITS_ADJUST_COMBAT_STRENGTH'),
+		('TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT', 'MODIFIER_PLAYER_UNITS_ADJUST_BARBARIAN_COMBAT');
+
+INSERT OR REPLACE INTO ModifierArguments
+		(ModifierId, Name, Value)
+	VALUES
+        -- ユニットの戦闘力 +5
+		('TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_COMBAT', 'Amount', 5),
+        -- ユニットの戦闘力(対蛮族) +50
+		('TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT', 'Amount', 50);
+
+INSERT OR REPLACE INTO TraitModifiers
+		(TraitType, ModifierId)
+	VALUES
+		('TRAIT_LEADER_SISTER_KOTONOHA', 'TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_COMBAT'),
+		('TRAIT_LEADER_SISTER_KOTONOHA', 'TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT');
+
+
+	-- <TraitModifiers>
+	-- 	<Row TraitType="TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE" ModifierId="TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_COMBAT"/>
+	-- 	<Row TraitType="TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE" ModifierId="TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT"/>
+	-- </TraitModifiers>
+
+	-- <Modifiers>
+	-- 	<Row>
+	-- 		<ModifierId>TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_COMBAT</ModifierId>
+	-- 		<ModifierType>MODIFIER_PLAYER_UNITS_ADJUST_COMBAT_STRENGTH</ModifierType>
+	-- 	</Row>
+	-- 	<Row>
+	-- 		<ModifierId>TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT</ModifierId>
+	-- 		<ModifierType>MODIFIER_PLAYER_UNITS_ADJUST_BARBARIAN_COMBAT</ModifierType>
+	-- 	</Row>
+	-- </Modifiers>
+
+	-- <ModifierArguments>
+	-- 	<Row>
+	-- 		<ModifierId>TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_COMBAT</ModifierId>
+	-- 		<Name>Amount</Name>
+	-- 		<Value>4</Value>
+	-- 	</Row>
+	-- 	<Row>
+	-- 		<ModifierId>TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT</ModifierId>
+	-- 		<Name>Amount</Name>
+	-- 		<Value>8</Value>
+	-- 	</Row>
+	-- </ModifierArguments>
+
+	-- <ModifierStrings>
+	-- 	<Row ModifierId="TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT" Context="Preview" Text="LOC_TRAIT_CHARLEMAGNE_SABER_JOYEUSE_ORDRE_EXTRA_COMBAT_DESCRIPTION"/>
+	-- </ModifierStrings>
